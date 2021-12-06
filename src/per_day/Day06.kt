5d4919f2ae741e47e8e44a6df2ad14e1123aa040
Alex Allafi <alexallafi.dev@gmail.com>
2021-12-06 18:53:05 +0200

    Solutions for Day06

---
+++ src/per_day/Day06.kt
@@ -0,0 +1,25 @@
+package per_day
+
+import java.io.File
+import java.util.*
+
+fun sol(input: List<String>, days: Int = 80): Long {
+
+    val lst = MutableList(10) { 0L }
+    input[0].split(',').map { it.toInt() }.forEach { lst[it] = lst[it].plus(1L) }
+
+    repeat(days) {
+        val toGiveBirth: Long = lst[0]
+        Collections.rotate(lst, -1)
+        lst[9] = 0
+        if (toGiveBirth > 0) {
+            lst[6] = lst[6] + toGiveBirth
+            lst[8] = lst[8] + toGiveBirth
+        }
+    }
+    return lst.sum()
+}
+
+fun main() {
+    println(sol(File("src", "inputs/Day06.txt").readLines(), 256))
+}
